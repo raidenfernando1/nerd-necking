09498a0ca0e8864fd677993e4f70080b1e58f57d
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2025-05-09 19:18:41 +0800

    removed unused useEffect

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { useShare } from "../context/useShare";
 import { useNavigate } from "react-router";
